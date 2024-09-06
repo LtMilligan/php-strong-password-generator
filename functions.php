3ef4e8c1a571d1982233b694e2dbff150531db8c
LtMilligan <gioele.miscia@gmail.com>
2024-09-06 16:35:22 +0200

    [UPD] Logic in functions.php and include in index.php

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php
 
+function passwordGenerator($length) {
+    $keys = array_merge(range(0, 9), range('A', 'z'));
+    
+    $psswrd = '';
+    for($i=0; $i < $length; $i++) {
+        $psswrd .= $keys[mt_rand(0, count($keys) - 1)];
+    }
+    return $psswrd;
+}
 
 ?>
